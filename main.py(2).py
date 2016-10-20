69fd5d8ff8f81a4d4d68b31926da20bc9c4b9caa
Miha Markovic <miha0552@gmail.com>
2016-10-20 20:24:50 +0200

    posodobljen pretvornik

--- main.py(2).py
+++ main.py(2).py
@@ -2,7 +2,7 @@ print("Zivijo")
 while True:
     prvo_vprasanje = int(raw_input("vnesi km: "))
     odgovor = prvo_vprasanje * 1.609344
-    print(odgovor)
+    print("To je " + str(odgovor) + " milj")
     drugo_vprasanje = raw_input("Zelite nadaljevati? ")
 
     if drugo_vprasanje == "ne":
