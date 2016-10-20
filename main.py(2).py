1c61f06dbdd1d91e5d8b86d08b2152f4425d61c4
Miha Markovic <miha0552@gmail.com>
2016-10-20 20:27:12 +0200

    še enkrat posodobljen pretvornik

--- main.py(2).py
+++ main.py(2).py
@@ -3,7 +3,7 @@ while True:
     prvo_vprasanje = int(raw_input("vnesi km: "))
     odgovor = prvo_vprasanje * 1.609344
     print("To je " + str(odgovor) + " milj")
-    drugo_vprasanje = raw_input("Zelite nadaljevati? ")
+    drugo_vprasanje = raw_input("Zelite nadaljevati?(da/ne) ")
 
     if drugo_vprasanje == "ne":
         print("Hvala za sodelovanje!!!")
